d068d1401db413c5aa9f65b1f4bd9ba3e7132129
bazinygero <97780599+bazinygero@users.noreply.github.com>
2022-08-15 14:43:10 +0300

    Add JS functional.

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,22 @@
-console.log('HELLO');
+const dropdownBtns = document.querySelectorAll('.dropdown-toggle');
 
-const test = () => {
-	console.log('this is a test');
-};
+dropdownBtns.forEach(btn => {
+    btn.addEventListener('click', event => {
+        event.preventDefault();
+        event.currentTarget.parentElement.classList.toggle('open');
+    })
+})
+
+const burgerBtn = document.querySelector('.burger-btn');
+const burgerBtnImg = document.querySelector('.burger-btn img');
+const burgerMenu = document.querySelector('.navbar-container');
+
+burgerBtn.addEventListener('click', event => {
+    burgerMenu.classList.toggle('show');
+
+    if (burgerMenu.classList.contains('show')) {
+        burgerBtnImg.src = './images/icon-close-menu.svg';
+    } else {
+        burgerBtnImg.src = './images/icon-menu.svg';
+    }
+})
